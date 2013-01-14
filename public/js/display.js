122e15145a85ddf9b136e0b1fcaeae703c662bc0
Stephen Thoma <modoatk@gmail.com>
2013-01-13 20:12:29 -0700

    externalize validation strategies

--- public/js/display.js
+++ public/js/display.js
@@ -162,63 +162,6 @@ function checkNumSelected(targetName, numNeededParam)
   	return numSelected >= numNeeded;
 }
 
-var validationStrategies = {};
-validationStrategies.step1 = [
-	{
-		method: checkNumSelected,
-		elementName: "#dropdown-grazingfeeds",
-		params: [2],
-		tooltip: "Need to select %d elements"
-	},
-	{
-		method: checkNumSelected,
-		elementName: "#dropdown-nongrazingfeeds",
-		params: [2],
-		tooltip: "Need to select %d elements"
-	}
-];
-
-validationStrategies.step2 = [
-  	{		
-		method: checkIfInRange,
-		elementName: "#wt-mature",
-		params: [400, 700],
-		tooltip: "Must be between %d and %d"
-  	},
-  	{
-		method: checkIfInRange,
-		elementName: "#num-calves-beef",
-		params: [0, 10000],
-		tooltip: "Must be between %d and %d"
-  	},
-  	{
-		method: checkIfInRange,
-		elementName: "#num-calves-dairy",
-		params: [0, 10000],
-		tooltip: "Must be between %d and %d"
-  	}
-];
-validationStrategies.step3 = [
-  	{
-		method: checkIfInRange,
-		elementName: "#wt-milk",
-		params: [0, 300000000],
-		tooltip: "Must be between %d and %d"
-  	},
-  	{
-		method: checkIfInRange,
-		elementName: "#pr-prot",
-		params: [0, 10],
-		tooltip: "Must be between %d and %d"
-  	},
-  	{
-		method: checkIfInRange,
-		elementName: "#pr-fat",
-		params: [0, 10],
-		tooltip: "Must be between %d and %d"
-  	}
-];
-
 /*
  * Evaluate a component's validation strategy.
  *
